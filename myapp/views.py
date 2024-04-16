674d635aaeeea21b1b1dacde2900b339c8ac6880
sabeehalam <sabeeh_alam_96@hotmail.com>
2024-04-16 13:27:22 +0300

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Feature
+
+# Create your views here.
+
+def index(request):
+    features = Feature.objects.all()
+    return render(request, 'index.html', {'features':features})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        repeat_password = request.POST['repeat_password']
+    
+        if password == repeat_password:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already used')   
+                return redirect('register') 
+            elif User.objects.filter(username = username).exists():
+                messages.info(request, "Username already exists")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username = username, email = email, password = password)
+                user.save()
+                return redirect('login')
+        else: 
+            messages.info(request, "Passwords do not match")
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials invalid')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def counter(request):
+    # text = request.POST['text']
+    # number_of_words = len(text.split())
+    posts = [1, 2, 3, 4, 'tim', 'john']
+    return render(request, 'counter.html', {'posts': posts}) #{'num_words': number_of_words},
+
+def post(request, pk):
+    return render(request, 'post.html', {'pk': pk})
